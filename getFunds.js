d8888a771e98930c7fc4b5c58ac25f1f9baf84fe
GlitchMasta47 <glitchmasta47@hotmail.com>
2018-06-07 16:27:33 -0500

    Hopefully we don't need to change this one either

---
+++ getFunds.js
@@ -0,0 +1,9 @@
+let counter = document.getElementById("current-fund");
+fetch("https://glitchmasta47.github.io/currentFund.txt")
+.then((res) => {
+  return res.text(); // We must return .text() instead of directly using .text() because it returns a promise instead of the text
+})
+.then((res) => {
+  // "res" is now equal to what was returned above (since it returns a promise and we wait for the ".then()" on the promise yada-yada)
+  counter.innerHTML = res;
+});
