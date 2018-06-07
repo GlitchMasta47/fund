1b5e4a69e229b30de9901bd12f27df3ab1e74d31
GlitchMasta47 <glitchmasta47@hotmail.com>
2018-06-07 16:59:36 -0500

    Ok so now it'll use this really cool animation

--- getFunds.js
+++ getFunds.js
@@ -1,9 +1,8 @@
-let counter = document.getElementById("current-fund");
 fetch("https://glitchmasta47.github.io/fund/currentFund.txt")
 .then((res) => {
   return res.text(); // We must return .text() instead of directly using .text() because it returns a promise instead of the text
 })
 .then((res) => {
   // "res" is now equal to what was returned above (since it returns a promise and we wait for the ".then()" on the promise yada-yada)
-  counter.innerHTML = res;
+  bounty.default({ el: '.current-fund', value: res, initialValue: '$0.00' })
 });
